f301e8074e3ece324e3f73b379f19cae0df805e8
mohaddis <mohaddiskhan05@gmail.com>
2021-05-05 17:00:08 +0530

    Added one method for getting employee by name

--- MVCProject1/src/main/java/com/mak/repository/CustomerRepository.java
+++ MVCProject1/src/main/java/com/mak/repository/CustomerRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.mak.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+	public Integer findByName();
 
 }
